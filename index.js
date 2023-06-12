2fe2eb173cba3db5cf96dcaffb99931ea36174a7
teksts <mark.egelhoff1@gmail.com>
2023-06-11 22:11:11 -0700

    Practicing with classes

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Transaction {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
+}
+
+class Deposit extends Transaction {
+
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.amount;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  commit() {
+    this.account.balance -= this.amount;
+  }
+
+}
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
   }
 
 }
@@ -18,12 +40,9 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account("snow-patrol");
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+let t1 = new Withdrawal(50.25, myAccount);
+t1.commit();
 
 console.log('Balance:', balance);
